1644beca865e6b4edd9fd9aa3bf4396cc7457b56
AnastasiA-Konovalova <nastia90709a@yandex.ru>
2025-05-18 01:51:15 +0300

    feat: добавлена предварительная структура проекта, реализован модуль статистики

--- stats/stats-server/src/main/resources/sсhema.sql
+++ stats/stats-server/src/main/resources/sсhema.sql
@@ -1,13 +1,13 @@
-CREATE TABLE IF NOT EXISTS endpoint_hits (
-    id SERIAL PRIMARY KEY,
-    app LONG NOT NULL,
-    uri VARCHAR(255) NOT NULL,
-    ip VARCHAR(255) NOT NULL,
-    act_time timestamp NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS apps (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    CONSTRAINT fk_app_endpoint FOREIGN KEY (id) REFERENCES endpoint_hits (app)
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS endpoint_hits (
+    id SERIAL PRIMARY KEY,
+    app INTEGER NOT NULL,
+    uri VARCHAR(255) NOT NULL,
+    ip VARCHAR(255) NOT NULL,
+    act_time timestamp NOT NULL,
+    CONSTRAINT fk_app_endpoint FOREIGN KEY (app) REFERENCES apps (id) ON DELETE CASCADE
 );
