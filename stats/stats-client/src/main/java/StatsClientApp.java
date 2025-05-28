b8f55e59d798a79ecec87d4aa1dd9d99c438939e
AnastasiA-Konovalova <nastia90709a@yandex.ru>
2025-05-28 22:43:29 +0300

    feat: добавлена предварительная структура проекта, реализован модуль статистики

--- stats/stats-client/src/main/java/StatsClientApp.java
+++ stats/stats-client/src/main/java/StatsClientApp.java
@@ -1,5 +1,8 @@
-import org.springframework.boot.SpringApplication;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class StatsClientApp {
     public static void main(String[] args) {
         SpringApplication.run(StatsClientApp.class, args);
